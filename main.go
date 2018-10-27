77a64da786935e5d0bf30f8d0ca0610fda1e7f52
huskyhoochu <dfg1499@gmail.com>
2018-10-27 09:21:21 +0900

    [#1] 기본 HtmlHandler 생성

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func HtmlHandler(w http.ResponseWriter, r *http.Request)  {
+	t := template.Must(template.ParseFiles("index.html"))
+	t.Execute(w, nil)
+}
 
 func main()  {
-	fmt.Println("hello")
+	log.Println("Server start on port 8000")
+	http.HandleFunc("/", HtmlHandler)
+	http.ListenAndServe(":8000", nil)
 }
