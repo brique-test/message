4505a82813f2d4e8840272d8cdfc09aeae3d1348
huskyhoochu <dfg1499@gmail.com>
2018-10-27 13:14:08 +0900

    [#1] MessageHandler 실행 위치 변경
    - ListenAndServe 함수 이전으로 위치를 옮겨서 MessageHandler가 실행되지 않던 문제 해결

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ func main() {
 	log.Println("Server start on port 8000")
 	http.HandleFunc("/", HtmlHandler)
 	http.HandleFunc("/ws", ConnHandler)
+
+	go MessageHandler()
+
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		log.Panicln(err)
@@ -17,6 +20,4 @@ func main() {
 		s := recover()
 		log.Println(s)
 	}()
-
-	go MessageHandler()
 }
