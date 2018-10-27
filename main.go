d7e047bb0a7a0b4d08eccdd316168ae7bbb38a84
huskyhoochu <dfg1499@gmail.com>
2018-10-27 10:38:44 +0900

    [#1] 서버 에러 발생 시 recover 함수를 사용해 복구할 수 있도록 설정

--- main.go
+++ main.go
@@ -8,5 +8,12 @@ import (
 func main()  {
 	log.Println("Server start on port 8000")
 	http.HandleFunc("/", HtmlHandler)
-	http.ListenAndServe(":8000", nil)
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer func() {
+		s := recover()
+		log.Println(s)
+	}()
 }
